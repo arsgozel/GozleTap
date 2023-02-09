44fbf1f6b3d113606a177c100ba04b3313660d5e
Gozel <30gozel30gozel@gmail.com>
2023-02-09 16:15:50 +0500

    1.9.2

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -4,15 +4,13 @@
 @endsection
 @section('content')
     <div class="container-xl p-0">
-        <div class="row gx-5">
-            <div class="col-6"><img src="{{ asset('img/main.png') }}" class="mx-0 px-0 text-start"></div>
-            <div class="col-6">
-                <div class="mt-5 pt-5 display-2 d-flex justify-content-center align-items-center text-center text-danger fw-semibold">@lang('app.app-name')</div>
-                <div class="fs-3 d-flex justify-content-center align-items-center text-center">Find Your Dream Job</div>
+        <div class="row align-items-center">
+            <div class="col-sm">
+                <div class="display-1 text-center text-danger fw-semibold">@lang('app.app-name')</div>
+                <div class="fs-3 text-center">Find Your Dream Job</div>
             </div>
+            <div class="col-sm"><img src="{{ asset('img/job_search2.png') }}" class="img-fluid py-2"></div>
         </div>
-
-
     </div>
 
 @endsection
